21cb945f4d5b62df7807c98dfbe446478150deba
Aldo Stracquadanio <aldo.strac@gmail.com>
2014-01-11 16:52:44 +0000

    Implement router

--- src/main/scala/org/lsug/restlife/Router.scala
+++ src/main/scala/org/lsug/restlife/Router.scala
@@ -1,8 +1,48 @@
 package org.lsug.restlife
 
-/**
- * Created by aldo on 11/01/14.
- */
-class Router {
+import spray.json._
+import spray.routing.HttpServiceActor
+import akka.actor.{Props, ActorSystem, Actor}
+import spray.httpx.SprayJsonSupport._
+import scala.annotation.tailrec
+import akka.io.IO
+import spray.can.Http
 
+case class BoardRequest(board: Board, steps: Int)
+
+object BoardJsonProtocol extends DefaultJsonProtocol {
+  implicit val cellFormat = jsonFormat2(Cell.apply _)
+  implicit val boardFormat = jsonFormat1(Board)
+  implicit val boardRequestFormat = jsonFormat2(BoardRequest)
+}
+
+class Router extends HttpServiceActor {
+  import BoardJsonProtocol._
+
+  @tailrec
+  private def runBoard(board: Board, steps: Int): Board =
+    if (steps == 0) board
+    else runBoard(board.nextGeneration, steps - 1)
+
+  def receive: Actor.Receive = runRoute {
+    post {
+      entity(as[BoardRequest]) { case BoardRequest(board, steps) =>
+        complete {
+          runBoard(board, steps)
+        }
+      }
+    }
+  }
+}
+
+object Main {
+  def main(args: Array[String]) {
+    implicit lazy val system = ActorSystem("game-of-life-system")
+
+    val router = system.actorOf(Props[Router], "router")
+
+    IO(Http) ! Http.Bind(router, interface = "localhost", port = 9909)
+
+    system.awaitTermination()
+  }
 }
